9bb9588d0573642f5e07172d40a767b5d0b516c5
yagodar <yagodar@qip.ru>
2013-09-12 12:35:35 +0400

    DbEditText. Теперь при pushToDb пустого значения, "пушится" значение столбца бд по умолчанию.

--- AndroidSQLiteManagerProject/AndroidSQLiteManager/src/main/java/com/yagodar/android/database/sqlite/custom/DbEditText.java
+++ AndroidSQLiteManagerProject/AndroidSQLiteManager/src/main/java/com/yagodar/android/database/sqlite/custom/DbEditText.java
@@ -78,6 +78,9 @@ public class DbEditText<T extends Object> extends EditText {
                 }
                 catch(Exception ignored){}
             }
+            else {
+                setDbValue(dbTableColumnBase.getDefValue());
+            }
         }
     }
 
